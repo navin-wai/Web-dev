aabe9548114120d3fc13b68a9274716b2a01cfa4
navin-wai <navinkumar677886@gmail.com>
2025-06-21 10:30:20 +0530

    A Random Color Generator
    i sample and easy to understand random color genrator

---
+++ randomColorGenerator/app.js
@@ -0,0 +1,21 @@
+let btn = document.querySelector('button');
+
+btn.addEventListener("click",function(){
+    let heading = document.querySelector('h1');
+    let randomcolor = rndColor();
+
+    heading.innerText = randomcolor;
+
+    let div = document.querySelector("div");
+    div.style.backgroundColor = randomcolor;
+    console.log("value updated!");
+
+})
+function rndColor(){
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+
+    let color = `rgb ${r},${g},${b}`;
+    return color;
+}
